dabd52e6a6b99d438221a21bc4faae06a13c1756
Lemuel Ray Lamela <lemuelraylamela123@gmail.com>
2025-10-31 08:32:21 +0800

    added loader for Account class page

--- app/components/sections/AccountClass.tsx
+++ app/components/sections/AccountClass.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "../ui/card";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
-import { Building2 } from "lucide-react";
+import { Building2, Loader2 } from "lucide-react";
 import { Skeleton } from "../ui/skeleton";
 
 interface AccountClass {
@@ -39,9 +39,8 @@ export default function AccountClass() {
         <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
           <div>
             <CardTitle className="flex items-center gap-2">
-              {" "}
-              <Building2 className="w-5 h-5 text-muted-foreground" /> Account
-              Classes{" "}
+              <Building2 className="w-5 h-5 text-muted-foreground" />
+              Account Classes
             </CardTitle>
           </div>
         </div>
@@ -50,10 +49,15 @@ export default function AccountClass() {
       <CardContent>
         <ScrollArea className="overflow-auto">
           {loading ? (
-            <div className="space-y-2">
-              {[...Array(4)].map((_, i) => (
-                <Skeleton key={i} className="h-6 w-full rounded-md" />
-              ))}
+            <div className="flex items-center justify-center h-[300px] w-full">
+              <div className="flex flex-col items-center gap-4">
+                <Loader2 className="h-6 w-6 animate-spin text-muted-foreground" />
+                <div className="space-y-2 w-[300px]">
+                  {[...Array(4)].map((_, i) => (
+                    <Skeleton key={i} className="h-6 w-full rounded-md" />
+                  ))}
+                </div>
+              </div>
             </div>
           ) : accountClasses.length === 0 ? (
             <p className="text-base text-muted-foreground">
@@ -63,8 +67,8 @@ export default function AccountClass() {
             <table className="w-full text-base border-collapse">
               <thead>
                 <tr className="bg-muted text-muted-foreground">
-                  <th className="text-left px-4 py-2 ">Account Class Code</th>
-                  <th className="text-left px-4 py-2 ">Account Class</th>
+                  <th className="text-left px-4 py-2">Account Class Code</th>
+                  <th className="text-left px-4 py-2">Account Class</th>
                 </tr>
               </thead>
               <tbody>
